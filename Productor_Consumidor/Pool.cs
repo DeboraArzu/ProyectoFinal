a8ef98f8fb26fe2ba7ef46a5e0d1e88a67574373
DeboraArzu <deboranise29@hotmail.com>
2017-05-07 15:57:03 -0600

    pool

--- Productor_Consumidor/Pool.cs
+++ Productor_Consumidor/Pool.cs
@@ -8,5 +8,28 @@ namespace Productor_Consumidor
 {
     class Pool
     {
+        public List<Worker> producer;
+        public List<Worker> consumer;
+        int maximo;
+        bool estado = false;
+
+        public int maxSize = 6;
+        //agregar comando sql
+        public RoundRobin RR;
+
+        private Pool(int producerparam, int consumerparam, int maximo)
+        {
+            RR = new RoundRobin();
+            this.producer = new List<Worker>();
+            this.consumer = new List<Worker>();
+            this.maximo = maximo;
+        }
+
+        bool agregar()
+        {
+            return estado;
+        }
+
+        //agregar intrucciones de SQL
     }
 }
